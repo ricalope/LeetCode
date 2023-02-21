ff756eb8cf4a3d28fdf71c0359ef53276e282427
Ricardo Lopez <102694854+ricalope@users.noreply.github.com>
2023-02-21 08:11:06 -0700

    Time: 95 ms (13.87%), Space: 42.9 MB (24.76%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    let count = 0
+    
+    while(n !== 0) {
+        let isOne = n & 1
+        if(isOne === 1) count++
+        n = n >>> 1
+    }
+    return count
+};

e81b835ac2943737e3adf63941e2a17c692b1068
Ricardo Lopez <102694854+ricalope@users.noreply.github.com>
2023-02-22 13:30:24 -0700

    Time: 108 ms (66.50%), Space: 48.7 MB (18.78%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    const all = [...nums1, ...nums2].sort((a,b) => a - b)
+    if(all.length % 2 !== 0) {
+        return all[Math.round(all.length - 1)/2]
+    }
+    const mid = all[(all.length/2) - 1]
+    const mid2 = all[(all.length/2)]
+    return (mid + mid2) / 2
+};

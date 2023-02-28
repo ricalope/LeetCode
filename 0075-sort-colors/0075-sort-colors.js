13a4e22f0d9469eb9eb8f12b7f44df22b3f8573d
Ricardo Lopez <102694854+ricalope@users.noreply.github.com>
2023-02-28 12:35:18 -0700

    Time: 54 ms (96.20%), Space: 42.6 MB (11.33%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,17 +3,19 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    if (!nums.length) return []
-    let pivot = nums[nums.length - 1]
-    let leftArr = []
-    let rightArr = []
-    for (let el of nums.slice(0, nums.length - 1)) {
-        el < pivot ? leftArr.push(el) : rightArr.push(el)
+    let l = 0;
+    let r = nums.length - 1
+    let i = 0
+    while(i <= r) {
+        if(nums[i] === 0) {
+            [nums[i], nums[l]] = [nums[l], nums[i]]
+            l++
+            i++
+        } else if(nums[i] === 2) {
+            [nums[i], nums[r]] = [nums[r], nums[i]]
+            r--
+        } else {
+            i++
+        }
     }
-    let abc = [...sortColors(leftArr), pivot, ...sortColors(rightArr)]
-    for(let i = 0; i < abc.length; i++) {
-        nums[i] = abc[i]
-    }
-    
-    return nums
 };

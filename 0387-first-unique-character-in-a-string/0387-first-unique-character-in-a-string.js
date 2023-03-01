ab2cb69dd844e178c3df5bbea1bbabd8da66347c
Ricardo Lopez <102694854+ricalope@users.noreply.github.com>
2023-03-01 10:10:42 -0700

    Time: 100 ms (82.83%), Space: 48.8 MB (19.27%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    let res = ''
+    const hash = s.split('').reduce((a,n) => {
+        a[n] ? a[n]++ : a[n] = 1
+        return a
+    }, {})
+    for(let key in hash) {
+        if(hash[key] === 1) {
+            res = key
+            break
+        }
+    }
+    return res === '' ? -1 : s.indexOf(res)
+};

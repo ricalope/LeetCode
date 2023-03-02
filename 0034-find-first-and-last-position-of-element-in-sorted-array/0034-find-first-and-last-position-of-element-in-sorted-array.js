73bfd52d054379b500cea89dc1217b8c7a5073de
Ricardo Lopez <102694854+ricalope@users.noreply.github.com>
2023-03-02 08:49:41 -0700

    Time: 57 ms (91.49%), Space: 42.4 MB (65.93%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    let i = 0, j = nums.length - 1, mid;
+    while(i <= j) {
+        mid = Math.floor((i + j) / 2)
+        nums[mid] >= target ? j = mid - 1 : i = mid + 1 
+    }
+    
+    if(nums[i] !== target) return [-1, -1]
+    
+    const start = i
+    
+    i = 0, j = nums.length - 1
+    
+    while(i <= j) {
+        mid = Math.floor((i + j) / 2)
+        nums[mid] <= target ? i = mid + 1 : j = mid - 1
+    }
+    return [start, j]
+};

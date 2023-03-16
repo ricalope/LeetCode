3e6c486fae19ed8128445349e545ce4983f6202e
Ricardo Lopez <102694854+ricalope@users.noreply.github.com>
2023-03-16 07:45:30 -0700

    Time: 66 ms (48.43%), Space: 42 MB (66.32%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let insert = m + n - 1
+    m--
+    n--
+    while(n >= 0) {
+        if(nums1[m] > nums2[n]) {
+            nums1[insert] = nums1[m]
+            m--
+        } else {
+            nums1[insert] = nums2[n]
+            n--
+        }
+        insert--
+    }
+};

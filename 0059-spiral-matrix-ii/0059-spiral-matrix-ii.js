cb81fa3e4a02b6f49cf5d6f8861d517bde84f489
Ricardo Lopez <102694854+ricalope@users.noreply.github.com>
2023-03-28 08:42:45 -0700

    Time: 54 ms (86.65%), Space: 42.3 MB (35.19%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.js
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.js
@@ -4,9 +4,9 @@
  */
 var generateMatrix = function(n) {
     let size = n * n, count = 0
-    let [left,right,top,bottom] = [0, n - 1, 0, n - 1]
+    let [left, right, top, bottom] = [0, n - 1, 0, n - 1]
     const res = new Array(n).fill(0).map(() => new Array(n).fill(0))
-
+    
     while(count < size) {
         for(let i = left; i <= right; i++) {
             count++

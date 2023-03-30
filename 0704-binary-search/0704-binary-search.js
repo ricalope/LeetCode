7ddbd94dd23599ba9324af2ff852148bdbeb83de
Ricardo Lopez <102694854+ricalope@users.noreply.github.com>
2023-03-30 16:53:00 -0700

    Time: 57 ms (91.02%), Space: 45.5 MB (9.32%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let start = 0, end = nums.length - 1
+    
+    while(start <= end) {
+        const mid = Math.floor((start + end) / 2)
+        
+        if(nums[mid] === target) return mid
+        if(target > nums[mid]) start = mid + 1
+        else end = mid - 1
+    }
+    return -1
+};

1c16aeae6511d6078b76a23eeee512d9475a33d4
Ricardo Lopez <102694854+ricalope@users.noreply.github.com>
2023-03-30 11:47:54 -0700

    Time: 70 ms (79.27%), Space: 44.3 MB (39.17%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} color
+ * @return {number[][]}
+ */
+var floodFill = function(image, sr, sc, color) {
+    const currColor = image[sr][sc]
+    const queue = [[sr,sc]]
+    if(currColor === color) return image
+    while(queue.length) {
+        const [cRow,cCol] = queue.shift()
+        if(image[cRow][cCol] === currColor) {
+            image[cRow][cCol] = color
+            
+            const neighbors = findNeighbors(image,cRow,cCol)
+            neighbors.forEach(nbr => {
+                queue.push(nbr)
+            })
+        }
+    }
+    return image
+};
+
+const findNeighbors = (grid,row,col) => {
+    const neighbors = []
+    if(row - 1 >= 0) neighbors.push([row - 1, col])
+    if(row + 1 < grid.length) neighbors.push([row + 1, col])
+    if(col - 1 >= 0) neighbors.push([row, col - 1])
+    if(col + 1 < grid[0].length) neighbors.push([row, col + 1])
+    return neighbors
+}

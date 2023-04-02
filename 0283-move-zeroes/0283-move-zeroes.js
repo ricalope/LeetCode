c61d11936bb491a9c5e2948655f3198877f868bc
Ricardo Lopez <102694854+ricalope@users.noreply.github.com>
2023-04-01 18:41:33 -0700

    Time: 103 ms (28.53%), Space: 47.3 MB (12.13%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -4,8 +4,8 @@
  */
 var moveZeroes = function(nums) {
     let i = 0
-    while (i < nums.length) {
-        if (nums[i] === 0) {
+    while(i <= nums.length) {
+        if(nums[i] === 0) {
             const num = nums.splice(i,1)
             nums.push(num)
         } else {

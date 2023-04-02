0e91841654223912361964f1e9f3b1a9ca7c2225
Ricardo Lopez <102694854+ricalope@users.noreply.github.com>
2023-04-01 18:53:08 -0700

    Time: 56 ms (91.87%), Space: 43.2 MB (40.08%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let i = 0, j = nums.length - 1
+    while(i < j) {
+        if(nums[i] + nums[j] === target) return [i + 1, j + 1]
+        else if(nums[i] + nums[j] < target) i++
+        else j--
+    }
+};

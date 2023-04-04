8af74fbfa6c63ccddd3362768cb458a0878d3d6f
Ricardo Lopez <102694854+ricalope@users.noreply.github.com>
2023-04-03 17:25:25 -0700

    Time: 56 ms (86.44%), Space: 42.7 MB (21.19%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -5,7 +5,7 @@
 var runningSum = function(nums) {
     const res = [nums[0]]
     for(let i = 1; i < nums.length; i++) {
-        res.push(res[i - 1] + nums[i])
+        res.push(nums[i] + res[i - 1])
     }
     return res
 };

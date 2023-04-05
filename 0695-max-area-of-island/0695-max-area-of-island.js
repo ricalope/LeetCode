b069f6e1e4eddc8214aae10dc3201fc5842d25bd
Ricardo Lopez <102694854+ricalope@users.noreply.github.com>
2023-04-05 16:41:26 -0700

    Time: 92 ms (34.31%), Space: 49.2 MB (20.33%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.js
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -3,34 +3,23 @@
  * @return {number}
  */
 var maxAreaOfIsland = function(grid) {
-    const visited = new Set()
-    let maxArea = 0
+    const res = { count: 0 }
     for(let r = 0; r < grid.length; r++) {
         for(let c = 0; c < grid[0].length; c++) {
-            let size = explore(grid,r,c,visited)
-            maxArea = Math.max(size, maxArea)
+            explore(grid,r,c,res)
         }
     }
-    return maxArea
+    return res.count
 };
 
-const explore = (grid,row,col,visited) => {
-    const ibRow = 0 <= row && row < grid.length
-    const ibCol = 0 <= col && col < grid[0].length
-    
-    if(!ibRow || !ibCol) return 0
-    if(grid[row][col] === 0) return 0
-    
-    const pos = [row,col].toString()
-    if(visited.has(pos)) return 0
-    visited.add(pos)
-    
-    let size = 1
-    const deltas = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-    for(let [dRow, dCol] of deltas) {
-        let nRow = row + dRow
-        let nCol = col + dCol
-        size += explore(grid,nRow,nCol,visited)
+const explore = (grid,row,col,res,area = { count: 0 }) => {
+    if(!grid[row] || !grid[row][col]) return
+    res.count = Math.max(res.count, area.count += grid[row][col])
+    grid[row][col] = 0
+    const deltas = [[1, 0],[-1, 0],[0, 1],[0, -1]]
+    for(let [dRow,dCol] of deltas) {
+        const nRow = row + dRow
+        const nCol = col + dCol
+        explore(grid,nRow,nCol,res,area)
     }
-    return size
 }

4090b20fd60e18dec5d2742c046441564035432b
Ricardo Lopez <102694854+ricalope@users.noreply.github.com>
2023-04-11 10:45:18 -0700

    Time: 81 ms (30.01%), Space: 44.7 MB (27.08%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.js
+++ 0733-flood-fill/0733-flood-fill.js
@@ -8,14 +8,11 @@
 var floodFill = function(image, sr, sc, color) {
     const currColor = image[sr][sc]
     if(currColor === color) return image
-    
     const queue = [[sr,sc]]
     while(queue.length) {
-        const [row, col] = queue.shift()
-        
+        let [row,col] = queue.shift()
         if(image[row][col] === currColor) {
             image[row][col] = color
-            
             const neighbors = findNeighbors(image,row,col)
             neighbors.forEach(nbr => {
                 queue.push(nbr)
@@ -25,11 +22,11 @@ var floodFill = function(image, sr, sc, color) {
     return image
 };
 
-const findNeighbors = (grid,r,c) => {
+const findNeighbors = (grid,row,col) => {
     const neighbors = []
-    if(r - 1 >= 0) neighbors.push([r - 1, c])
-    if(r + 1 < grid.length) neighbors.push([r + 1, c])
-    if(c - 1 >= 0) neighbors.push([r, c - 1])
-    if(c + 1 < grid[0].length) neighbors.push([r, c + 1])
+    if(row - 1 >= 0) neighbors.push([row - 1, col])
+    if(row + 1 < grid.length) neighbors.push([row + 1, col])
+    if(col - 1 >= 0) neighbors.push([row, col - 1])
+    if(col + 1 < grid[0].length) neighbors.push([row, col + 1])
     return neighbors
 }

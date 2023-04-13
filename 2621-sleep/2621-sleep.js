36ff7fd73056f6c7357cb89a5de95c4be41c8f96
Ricardo Lopez <102694854+ricalope@users.noreply.github.com>
2023-04-13 06:37:20 -0700

    Time: 70 ms (50.00%), Space: 42.1 MB (50.00%) - LeetHub

---
+++ 2621-sleep/2621-sleep.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+    const p = new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve()
+        }, millis)
+    })
+    return p
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */

1647fc6f3dbff77d8d80e43b9cec852e83013353
Ricardo Lopez <102694854+ricalope@users.noreply.github.com>
2023-04-15 08:17:03 -0700

    Time: 49 ms (98.78%), Space: 45.6 MB (7.54%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -5,13 +5,11 @@
  */
 var search = function(nums, target) {
     let start = 0, end = nums.length - 1
-    
     while(start <= end) {
         const mid = Math.floor((start + end) / 2)
-        
         if(nums[mid] === target) return mid
-        if(target > nums[mid]) start = mid + 1
-        if(target < nums[mid]) end = mid - 1
+        if(nums[mid] < target) start = mid + 1
+        if(nums[mid] > target) end = mid - 1
     }
     return -1
 };
